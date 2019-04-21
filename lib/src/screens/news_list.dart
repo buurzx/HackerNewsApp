cbf70a0b14ec97aed9e7327648103397482728e6
Pavel Ro <buurzx@gmail.com>
2019-04-21 18:17:47 +0300

    Added StoriesBloc

--- lib/src/screens/news_list.dart
+++ lib/src/screens/news_list.dart
@@ -1,12 +1,37 @@
 import 'package:flutter/material.dart';
+import '../blocs/stories_provider.dart';
 
 class NewsList extends StatelessWidget {
   Widget build(BuildContext context) {
+    final bloc = StoriesProvider.of(context);
+
+    // TEMP , BADD
+    bloc.fetchTopIds();
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Top News'),
       ),
-      body: Text('Here show the news'),
+      body: buildlList(bloc),
+    );
+  }
+
+  Widget buildlList(StoriesBloc bloc) {
+    return StreamBuilder(
+      stream: bloc.topIds,
+      builder: (contex, AsyncSnapshot<List<int>> snapshot) {
+        if (!snapshot.hasData) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        return ListView.builder(
+          itemCount: snapshot.data.length,
+          itemBuilder: (context, int index) {
+            return Text('${snapshot.data[index]}');
+          },
+        );
+      },
     );
   }
 }
